7cdc07db8beb44304879f6796c58d9495373ca85
Kimchhun Tan <kimchhun.tan@quad.co.jp>
2022-06-15 17:15:40 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h3>{{ name }}</h3>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import {reactive, toRefs} from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const state = reactive({
+      name: process.env.VUE_APP_NAME
+    })
+
+    return {
+      ...toRefs(state)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
