fd8906f385111551ac3bc330bec2facdb8907a0c
Kimchhun Tan <kimchhun.tan@quad.co.jp>
2022-06-16 11:53:01 +0900

    feat: distinguish branch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <h3>{{ name }}</h3>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App From Main"/>
 </template>
 
 <script>
